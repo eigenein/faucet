b951cddb03e752015da5b6ca70564b17bb126dfe
Pavel Perestoronin <eigenein@gmail.com>
2016-01-28 01:06:05 +0100

    Got transactions working.

--- faucet.py
+++ faucet.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import decimal
+import hashlib
+import hmac
 import http
+import json
 import logging
 import pathlib
 import pickle
 import time
+import typing
 
 import click
 import redis
+import tornado.gen
+import tornado.httpclient
 import tornado.ioloop
 import tornado.template
 import tornado.web
 
 
 class Configuration:
-    # Coinbase API endpoints.
-    COINBASE_API_ENDPOINT = "https://api.coinbase.com/v2/"
-    COINBASE_API_ENDPOINT_SANDBOX = "https://api.sandbox.coinbase.com/"
-    # Coinbase API key.
-    COINBASE_API_KEY = "dOjIYwTJClFK7pt6"
-    COINBASE_API_SECRET = "yHnBgl62iUkpVGEs6NTUCoabejWsyP1D"
+    # Coinbase API production.
+    # TODO: COINBASE_API_KEY = "dOjIYwTJClFK7pt6"
+    # TODO: COINBASE_API_SECRET = b"yHnBgl62iUkpVGEs6NTUCoabejWsyP1D"
+    # TODO: COINBASE_API_TRANSACTIONS_PATH = "/v2/accounts/ac5b48f8-648e-5668-bf9f-6b8e980fe2e1/transactions"
+    # TODO: COINBASE_API_TRANSACTIONS_URL = "https://api.sandbox.coinbase.com" + COINBASE_API_TRANSACTIONS_PATH
+    # Coinbase API sandbox.
+    COINBASE_API_KEY = "PmfEFl20TP27qoLB"
+    COINBASE_API_SECRET = b"68UgM07f3XIK49Eglj0BAVfxFSoyR4qH"
+    COINBASE_API_TRANSACTIONS_PATH = "/v2/accounts/ac5b48f8-648e-5668-bf9f-6b8e980fe2e1/transactions"
+    COINBASE_API_TRANSACTIONS_URL = "https://api.sandbox.coinbase.com" + COINBASE_API_TRANSACTIONS_PATH
+    # Coinbase other.
+    COINBASE_API_VERSION = "2016-01-27"
+    COINBASE_API_MINIMUM_AMOUNT_BITS = 100
     # Cookies.
     COOKIE_LAST_EARN_TIMESTAMP = "let"
     COOKIE_SECRET = b"One wallet to rule them all"
@@ -34,21 +48,24 @@ class Configuration:
         click.style("%(message)s", bold=True),
     ))
     # Game balance.
-    EARN_WAITING_TIME_MINUTES = 1
+    EARN_WAITING_TIME_MINUTES = 1  # TODO: adjust.
     EARN_WAITING_TIME = 60.0 * EARN_WAITING_TIME_MINUTES
+    EARN_AMOUNT_BITS = 1  # TODO: adjust.
     # Redis.
     REDIS_EARN_TIME_EXPIRE = 24 * 60 * 60
     REDIS_EARN_TIME_KEY_FORMAT = "faucet:%s:earn_time"
+    REDIS_BALANCE_KEY_FORMAT = "faucet:%s:balance"
+    REDIS_BALANCE_EXPIRE = 30 * 24 * 60 * 60
 
 
 class Application(tornado.web.Application):
 
-    def __init__(self, database: redis.StrictRedis):
+    def __init__(self, database: redis.StrictRedis, http_client: tornado.httpclient.AsyncHTTPClient):
         favicon_path = pathlib.Path(__file__).absolute().parent / "favicomatic"
 
         super().__init__(
             [
-                (r"/", HomeRequestHandler, {"database": database}),
+                (r"/", HomeRequestHandler, {"database": database, "http_client": http_client}),
                 (r"/((apple-touch-icon|favicon|mstile).*)", tornado.web.StaticFileHandler, {"path": str(favicon_path)}),
             ],
             cookie_secret=Configuration.COOKIE_SECRET,
@@ -58,69 +75,147 @@ class Application(tornado.web.Application):
         )
 
 
+# noinspection PyAbstractClass
 class HomeRequestHandler(tornado.web.RequestHandler):
 
+    bits_in_btc = decimal.Decimal(1000000)
+
     # noinspection PyMethodOverriding
-    def initialize(self, database: redis.StrictRedis):
+    def initialize(self, database: redis.StrictRedis, http_client: tornado.httpclient.AsyncHTTPClient):
         # noinspection PyAttributeOutsideInit
         self.database = database
+        # noinspection PyAttributeOutsideInit
+        self.http_client = http_client
+
+    def render(self, wallet_address="", balance=None, **kwargs):
+        super().render(
+            "home.html",
+            Configuration=Configuration,
+            wallet_address=wallet_address,
+            balance=balance,
+            **kwargs
+        )
 
     @tornado.web.removeslash
     def get(self):
-        self.handle(self.get_cookie_waiting_time())
+        # TODO: set referrer cookie.
+        self.render(waiting_time=self.get_cookie_waiting_time())
 
+    @tornado.gen.coroutine
     def post(self):
         if self.get_body_argument("c") != "js":
             # This is either a robot or a human with disabled JavaScript.
             raise tornado.web.HTTPError(http.HTTPStatus.BAD_REQUEST.value, "Are you a bot?")
 
+        # Let's initialize the wallet info.
         wallet_address = self.get_body_argument("wallet_address")
-        wallet_key = Configuration.REDIS_EARN_TIME_KEY_FORMAT % wallet_address
+        wallet_earn_time_key = Configuration.REDIS_EARN_TIME_KEY_FORMAT % wallet_address
+        wallet_balance_key = Configuration.REDIS_BALANCE_KEY_FORMAT % wallet_address
+        balance = self.safe_loads(self.database.get(wallet_balance_key), 0)
 
         # Check waiting time.
         waiting_time = self.get_cookie_waiting_time()
         if waiting_time > 0.0:
-            self.handle(waiting_time)
+            self.render(waiting_time=waiting_time, wallet_address=wallet_address, balance=balance)
             return
-        wallet_cookie = self.database.get(wallet_key)
-        if wallet_cookie:
-            waiting_time = self.get_waiting_time(pickle.loads(wallet_cookie))
-            if waiting_time > 0.0:
-                self.handle(waiting_time)
-                return
+        waiting_time = self.get_waiting_time(self.safe_loads(self.database.get(wallet_earn_time_key), 0.0))
+        if waiting_time > 0.0:
+            self.render(waiting_time=waiting_time, wallet_address=wallet_address, balance=balance)
+            return
 
-        # TODO: send money.
-        logging.info("Send money to %s.", wallet_address)
+        # All checks passed. Increment balance.
+        logging.info("Incrementing balance for %s (was %s Bits).", wallet_address, balance)
+        balance += Configuration.EARN_AMOUNT_BITS
+        # Save the balance to be on safe side.
+        self.database.set(wallet_balance_key, pickle.dumps(balance), ex=Configuration.REDIS_BALANCE_EXPIRE)
+        if balance >= Configuration.COINBASE_API_MINIMUM_AMOUNT_BITS:
+            # Balance is large enough. Send money.
+            logging.info("Sending %s Bits to %s…", balance, wallet_address)
+            if (yield self.send_money(wallet_address, balance)):
+                # Success.
+                self.database.delete(wallet_balance_key)
+                balance = 0
 
         # Remember last earn time for the wallet and cookie.
         cookie = pickle.dumps(time.time())
-        self.database.set(wallet_key, cookie, ex=Configuration.REDIS_EARN_TIME_EXPIRE)
+        self.database.set(wallet_earn_time_key, cookie, ex=Configuration.REDIS_EARN_TIME_EXPIRE)
         self.set_secure_cookie(Configuration.COOKIE_LAST_EARN_TIMESTAMP, cookie)
 
-        # Render the page. We now that we've just sent money.
-        self.handle(Configuration.EARN_WAITING_TIME)
-
-    def handle(self, waiting_time: float):
-        """
-        We return the same response for both GET and POST.
-        This method contains the shared logic.
-        """
+        # Render the page. We know that we've just sent money.
         self.render(
-            "home.html",
-            Configuration=Configuration,
-            waiting_time=waiting_time,
+            waiting_time=Configuration.EARN_WAITING_TIME,
+            wallet_address=wallet_address,
+            balance=balance,
+            # TODO: pass success/error flag to render a notification.
         )
 
     def get_cookie_waiting_time(self) -> float:
         """
         Reads the cookie and gets remaining wait time.
         """
-        cookie = self.get_secure_cookie(Configuration.COOKIE_LAST_EARN_TIMESTAMP)
-        return self.get_waiting_time(pickle.loads(cookie) if cookie else 0)
+        return self.get_waiting_time(self.safe_loads(
+            self.get_secure_cookie(Configuration.COOKIE_LAST_EARN_TIMESTAMP), 0))
 
     @staticmethod
     def get_waiting_time(earn_time: float) -> float:
         return earn_time + Configuration.EARN_WAITING_TIME - time.time()
+
+    @staticmethod
+    def safe_loads(bytes_object: typing.Optional[bytes], default):
+        return pickle.loads(bytes_object) if bytes_object is not None else default
+
+    @tornado.gen.coroutine
+    def send_money(self, wallet_address: str, amount_bits: int):
+        request_body = json.dumps({
+            "type": "send",
+            "to": wallet_address,
+            "amount": str(decimal.Decimal(amount_bits) / self.bits_in_btc),
+            "currency": "BTC",
+            "description": "Your free Bitcoins from https://bitcoin.eigenein.xyz – enjoy!",
+            "skip_notifications": True,
+        })
+        timestamp = str(int(time.time()))
+
+        http_response = yield self.http_client.fetch(
+            tornado.httpclient.HTTPRequest(
+                Configuration.COINBASE_API_TRANSACTIONS_URL,
+                method="POST",
+                headers={
+                    "Content-Type": "application/json",
+                    "CB-VERSION": Configuration.COINBASE_API_VERSION,
+                    "CB-ACCESS-KEY": Configuration.COINBASE_API_KEY,
+                    "CB-ACCESS-TIMESTAMP": timestamp,
+                    "CB-ACCESS-SIGN": hmac.new(
+                        Configuration.COINBASE_API_SECRET,
+                        msg=("%sPOST%s%s" % (
+                            timestamp,
+                            Configuration.COINBASE_API_TRANSACTIONS_PATH,
+                            request_body,
+                        )).encode("utf-8"),
+                        digestmod=hashlib.sha256,
+                    ).hexdigest(),
+                },
+                body=request_body,
+            ),
+            raise_error=False,
+        )
+        response_body = http_response.body.decode("utf-8")
+
+        if http_response.code in (http.HTTPStatus.OK.value, http.HTTPStatus.CREATED.value):
+            response = json.loads(response_body)
+            logging.info("Successfully sent money to %s. Transaction %s.", wallet_address, response["data"]["id"])
+            return True
+
+        logging.error(
+            "Error while sending money to %s: %s %s",
+            wallet_address, http_response.code, http_response.reason
+        )
+        logging.error("Headers: %s", list(http_response.request.headers.get_all()))
+        logging.error("Request: %s", request_body)
+        logging.error("Response: %s", response_body)
+        return False
+
+
 
 
 @click.command()
@@ -138,7 +233,7 @@ def main(log_file, verbose: bool):
     )
 
     logging.info("Starting application on port %s…", Configuration.HTTP_PORT)
-    Application(redis.StrictRedis()).listen(Configuration.HTTP_PORT)
+    Application(redis.StrictRedis(), tornado.httpclient.AsyncHTTPClient()).listen(Configuration.HTTP_PORT)
     tornado.ioloop.IOLoop.current().start()
 
 
